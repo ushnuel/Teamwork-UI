6b4a18bf12fdb62d6d8d697cebcf69637c1b15ba
Emmanuel <ezeemmanuel2010@gmail.com>
2019-11-29 00:43:15 +0100

    Updates

---
+++ precache-manifest.d3419b7efa7e14a4fd7fbcd922827d4d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdfbb5b2255d0bb45e0bec82146db5f",
+    "url": "/Teamwork-UI/index.html"
+  },
+  {
+    "revision": "e8c4027af893502f7688",
+    "url": "/Teamwork-UI/static/css/main.8d516285.chunk.css"
+  },
+  {
+    "revision": "29fb09b66c6e5b237668",
+    "url": "/Teamwork-UI/static/js/2.629668e2.chunk.js"
+  },
+  {
+    "revision": "e8c4027af893502f7688",
+    "url": "/Teamwork-UI/static/js/main.e800a19a.chunk.js"
+  },
+  {
+    "revision": "4b5321f8ea99bbfab081",
+    "url": "/Teamwork-UI/static/js/runtime-main.80c83bfb.js"
+  },
+  {
+    "revision": "fcd245f273acca0dc0c9e47f51deddec",
+    "url": "/Teamwork-UI/static/media/tm-collaborate.fcd245f2.png"
+  },
+  {
+    "revision": "e4835f895bfc1b4da56186b61f143910",
+    "url": "/Teamwork-UI/static/media/tm-ethics.e4835f89.png"
+  },
+  {
+    "revision": "3980442aeece7bca7602d6571e4e474b",
+    "url": "/Teamwork-UI/static/media/tm-excellence.3980442a.png"
+  },
+  {
+    "revision": "4cdf8ff704bc95234567698e3c873caa",
+    "url": "/Teamwork-UI/static/media/tm-passion.4cdf8ff7.png"
+  },
+  {
+    "revision": "d80ae335e859693fa13c2b6b8dc8485b",
+    "url": "/Teamwork-UI/static/media/tm_network.d80ae335.png"
+  }
+]);
