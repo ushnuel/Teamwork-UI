ab97684f9aa5f0feb401fa0b977e21da7503e139
Emmanuel <ezeemmanuel2010@gmail.com>
2019-11-29 10:43:23 +0100

    chore(deploy): deploy app

--- teamwork/src/App.js
+++ teamwork/src/App.js
@@ -19,7 +19,7 @@ import DeleteGif from './Components/Gif/deleteGif';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename='/'>
         <Layout>
           <Switch>
             <Route path='/' exact component={Home} />
